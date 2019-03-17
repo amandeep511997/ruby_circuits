4262a5c24866a2e36f7b2382ce8fc3c2fc7f2f10
Amandeep Singh <amandeep511997@gmail.com>
2019-03-17 13:28:37 +0530

    Added operations on binary numbers

--- lib/ruby_ciruits/digital_circuits/circuit_helpers/operations/binary_operations.rb
+++ lib/ruby_ciruits/digital_circuits/circuit_helpers/operations/binary_operations.rb
@@ -12,33 +12,57 @@
 class BinaryOperations
 
 	def add(input1, input2)
-		parsed_input1, parsed_input2 = parsed_inputs(input1, input2)
-
+		x, y = parsed_inputs(input1, input2)
+		z = (x.to_i(2) + y.to_i(2)).to_s(2)
+		if z[0] == '-'
+			return z.slice(1, z.length)
+		end
+		return z
 	end
 	
 	def subtract(input1, input2)
-		parsed_input1, parsed_input2 = parsed_inputs(input1, input2)
+		x, y = parsed_inputs(input1, input2)
+		z = (x.to_i(2) - y.to_i(2)).to_s(2)
+		if z[0] == '-'
+			return z.slice(1, z.length)
+		end
+		return z
 	end
 
 	def multiply(input1, input2)
-		parsed_input1, parsed_input2 = parsed_inputs(input1, input2)
+		x, y = parsed_inputs(input1, input2)
+		z = (x.to_i(2) * y.to_i(2)).to_s(2)
+		if z[0] == '-'
+			return z.slice(1, z.length)
+		end
+		return z
 	end
 
 	def divide(input1, input2)
-		parsed_input1, parsed_input2 = parsed_inputs(input1, input2)
+		x, y = parsed_inputs(input1, input2)
+		z = (x.to_i(2) / y.to_i(2)).to_s(2)
+		if z[0] == '-'
+			return z.slice(1, z.length)
+		end
+		return z
 	end
 
 	def complement(input, option)
-		parsed_input = parse_input(input)
-		
+		x = parse_input(input)
+		z = (x.to_i(2) ^ ('1' * input.length).to_i(2)).to_s(2)
+		op = (z.to_i(2) + '1'.to_i(2)).to_s(2)
+		if option == '1'
+			return (((input.length - z.length) * '0') + z)
+		end
+		return (((input.length - op.length) * op[0]) + op)
 	end
 
 	def self.dec_to_bin(number)
-
+		#todo
 	end
 
 	def self.bin_to_dec(number)
-
+		#todo
 	end
 private
 
--- lib/ruby_ciruits/digital_circuits/circuit_helpers/utility/binary_tools.rb
+++ lib/ruby_ciruits/digital_circuits/circuit_helpers/utility/binary_tools.rb
@@ -3,5 +3,5 @@
 =end
 
 module BinaryTools
-
+	
 end
